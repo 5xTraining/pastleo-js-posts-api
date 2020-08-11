953e128326248fa083e99372a6e2d5001ba87435
PastLeo <chgu82837@gmail.com>
2020-08-11 13:44:13 +0800

    prevent too much posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   validates :title, :author, :description, :content, presence: true
+
+  after_create :destroy_old_posts_if_too_many
+
+  POST_COUNT_LIMIT = 1000
+  def destroy_old_posts_if_too_many
+    if Post.count > POST_COUNT_LIMIT
+      Post.order(id: :asc).limit(Post.count - POST_COUNT_LIMIT).destroy_all
+    end
+  end
 end
